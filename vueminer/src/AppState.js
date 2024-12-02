2679e5a822605ff2715c12db179d2a323bf8d166
Michael Giesler <michael.giesler1@icloud.com>
2024-12-02 14:19:27 -0700

    created basic game state

--- vueminer/src/AppState.js
+++ vueminer/src/AppState.js
@@ -1,10 +1,8 @@
 import { reactive } from 'vue'
 
-// NOTE AppState is a reactive object to contain app level data
+
 export const AppState = reactive({
-  /**@type {import('@bcwdev/auth0provider-client').Identity} */
-  identity: null,
-  /** @type {import('./models/Account.js').Account} user info from the database*/
-  account: null
+  cheese: 0,
+  totalCollected: 0,
 })
 
