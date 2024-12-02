12efeb268dc429a47e31792119776e9eaa8eb2b6
Michael Giesler <michael.giesler1@icloud.com>
2024-12-02 14:56:07 -0700

    edited homepage.vue and app.vue to remove default codeworks stylings

--- vueminer/src/pages/HomePage.vue
+++ vueminer/src/pages/HomePage.vue
@@ -1,37 +1,16 @@
 <script setup>
-
+import MoonMiner from '../components/MoonMiner.vue'
 </script>
 
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 card align-items-center shadow rounded elevation-3">
-      <img src="@/assets/img/cw-circle-logo.png" alt="CodeWorks Logo"
-        class="rounded-circle">
-      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        Vue 3 Starter
-      </h1>
-    </div>
+  <div class="home">
+    <MoonMiner />
   </div>
 </template>
 
 <style scoped lang="scss">
 .home {
-  display: grid;
-  height: 80vh;
-  place-content: center;
-  text-align: center;
-  user-select: none;
-
-  .home-card {
-    width: clamp(500px, 50vw, 100%);
-
-    >img {
-      height: 200px;
-      max-width: 200px;
-      width: 100%;
-      object-fit: contain;
-      object-position: center;
-    }
-  }
+  width: 100%;
+  height: 100%;
 }
 </style>
